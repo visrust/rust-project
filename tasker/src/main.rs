8d03827777fd59c0678d972b71fbf19060260dfd
nvrush <madharchodambani@gmail.com>
2026-01-27 10:36:52 +0530

    fixed tasker bug

--- tasker/src/main.rs
+++ tasker/src/main.rs
@@ -1,6 +1,9 @@
-use std::{fs::File, io::{self, Write}};
+use std::{
+    fs::File,
+    io::{self, Write},
+};
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Welcome to tasker");
 
     print!("Enter name of Task file : ");
@@ -9,22 +12,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     let mut file_name = String::new();
     io::stdin().read_line(&mut file_name)?;
 
+    let file = file_name.trim();
 
-    let space_char = " ".as_bytes();
-
-    if file_name.as_bytes() == space_char {
-        println!("Name can't have space")
-    } else if file_name.contains(".") {
+    if file.is_empty() {
+        println!("File name can't be empty");
+    } else if file.contains(".") {
         println!("Adding .extension is not allowed")
     } else {
-        let mut note = File::create_new(file_name)?;
+        let full_path = format!("{}.txt", file);
+        let mut note = File::create_new(&full_path)?;
         print!("Enter your todo: ");
         io::stdout().flush()?;
 
         let mut todo = String::new();
         io::stdin().read_line(&mut todo)?;
-        
-        let _write1 = writeln!(note, "{}",todo);
+
+        let _write1 = writeln!(note, "{}", todo);
     }
 
     Ok(())
